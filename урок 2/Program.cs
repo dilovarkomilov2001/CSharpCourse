c15f2db5c7bebdceb76b5734aaddfadb43a5a760
User <User@WIN-C3DCQQTMRJB>
2024-02-25 16:02:05 +0500

    Добавьте файлы проекта.

---
+++ урок 2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("salom!");
+string salom = Console.ReadLine();
+
+Console.WriteLine("nom va nasab!");
+string nomvanasab = Console.ReadLine();
+Console.WriteLine("salom " + nomvanasab);
+Console.WriteLine("Mehohed ki ba banki mo mablag guzored!");
+Console.ReadLine();
+Console.WriteLine("Deposit mo dar yk moh 7% ast!");
+Console.WriteLine("chan qadar mehohed deposit kuned?");
+double summai_deposit = double.Parse(Console.ReadLine());//100
+
+double mohiYakkumSumma = summai_deposit * 1.07;
+Console.WriteLine($"Mohi yakum deposit: {mohiYakkumSumma}");
+
+double mohiduyumsumma = mohiYakkumSumma * 1.07;
+Console.WriteLine($"Mohi duyum: {mohiduyumsumma}");
+
+double mohisevum = mohiduyumsumma * 1.07;
+Console.WriteLine($"mohi sevum: {mohisevum}");
+Console.WriteLine($"hamagi foida:{mohisevum-summai_deposit}");
+
+
+
+
+
+
+
+
+bool nomi_durust = false;
+short short_nomber = 32767;//2 bit 
+double double_nomber = 10.5;
